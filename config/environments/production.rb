05f0ab791b7c74fd5ad8bef48440b4aa4199d185
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-12-09 17:06:37 +0300

    set up cors auth

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts <<"my-backend-production.up.railway.app"
+  config.hosts << "my-backend-production-cc33.up.railway.app"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
